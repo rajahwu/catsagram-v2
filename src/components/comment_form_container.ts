b4109d066aa230849f10917061ddc2b20781bbb6
rajahwu <vmradford@gmail.com>
2023-01-24 09:41:18 -0500

    add comment form reset button

--- src/components/comment_form_container.ts
+++ src/components/comment_form_container.ts
@@ -1,7 +1,7 @@
 import $ from "jquery";
 import Comment from "./comment";
 
-const CommentFormContainer:JQuery<HTMLDivElement> = $('<div>', {
+const CommentFormContainer = $('<div>', {
     width: '350px',
     height: '200px',
     id: 'comment_form_container',
@@ -18,6 +18,10 @@ const CommentFormContainer:JQuery<HTMLDivElement> = $('<div>', {
         backgroundColor: 'hsl(0, 0%, 5%)'
     }
 })
+.data("initialX", 0)
+.data("initialY", 0)
+.data("moveElement", false)
+
 
 CommentFormContainer.hide()
 
@@ -41,14 +45,47 @@ const newComment: JQuery<HTMLTextAreaElement> = $('<input>', {
     height: '100px',
 })
 
-const submitBtn: JQuery<HTMLButtonElement> = $('<button>', {
+const formBtnContainer = $('<div>', {
+    width: '25px',
+    height: '50px',
+    css: {
+        padding: '0px',
+        placeItems: 'center'
+    }
+})
+
+const resetBtn = $('<button>', {
+    width: '25px',
+    height: '25px',
+    css: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '50%',
+        alignSelf: 'end',
+        marginBottom: '20px',
+        marginLeft: '7px'
+    },
+    click: (e: Event) => {
+        e.preventDefault()
+        CommentFormContainer.hide()
+        newComment.val('')
+    }
+})
+.append($('<img>', {
+    width: 25,
+    height: 25,
+    'src': '../red_x.png'
+}))
+
+const submitBtn = $('<button>', {
     width: '25px',
     height: '25px',
     css: {
         borderRadius: '50%',
         alignSelf: 'end',
         marginBottom: '20px',
-        marginLeft: '7px'
+        marginLeft: '7px',
     },
     click: (e: Event) => {
         e.preventDefault()
@@ -60,9 +97,15 @@ const submitBtn: JQuery<HTMLButtonElement> = $('<button>', {
         newComment.val('')
     }
 })
-
+.append($('<img>', {
+    width: 10,
+    height: 10,
+    'src': '../green_check.png'
+}))
 commentForm.append(newComment)
-commentForm.append(submitBtn)
+commentForm.append(formBtnContainer)
+formBtnContainer.append(resetBtn)
+formBtnContainer.append(submitBtn)
 CommentFormContainer.append(commentForm)
 
 export default CommentFormContainer;
